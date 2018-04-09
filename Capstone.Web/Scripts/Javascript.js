f079628ca1897ad5ad05af31db915e0eb1160432
Michael Weiss <mweis9676@gmail.com>
2018-04-09 17:30:00 -0400

    age restriction added javascript

---
+++ Capstone.Web/Scripts/Javascript.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $('#ageSubmit').click(function () {
+
+        let date = new Date($('.ageCheckerForm').val());
+
+        let birthday = Math.abs(date.getTime());
+
+        let today = Date.now();
+
+        let difference = Math.floor((today - birthday) / 31556952000);
+
+        if (difference >= 21) {
+            $('.ageCheckerDiv').css({
+                zIndex: '-10'
+            });
+        } else {
+            alert("You must be 21 years old to view this site.");
+        }
+    });
+});
