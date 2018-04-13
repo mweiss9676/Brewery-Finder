4aa9b960913852cea6405a8f2ead1c34dfeaa00c
Michael Weiss <mweis9676@gmail.com>
2018-04-13 10:30:19 -0400

    brewer controller added, addbeer action added, greatsuccess (beer added) view added, addbeermodel added, addnewbeer method added to beerdal

---
+++ Capstone.Web/Controllers/BrewerController.cs
@@ -0,0 +1,40 @@
+﻿using Capstone.Web.DAL;
+using Capstone.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capstone.Web.Controllers
+{
+    public class BrewerController : Controller
+    {
+
+        private IBeerDAL beerDAL;
+
+        public BrewerController(IBeerDAL beerDAL)
+        {
+            this.beerDAL = beerDAL;
+        }
+
+        // GET: Brewer
+        public ActionResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpPost]
+        public ActionResult AddBeer(AddBeerModel beer)
+        {
+            beerDAL.AddNewBeer(beer);
+            return RedirectToAction("GreatSuccess");
+        }
+
+        public ActionResult GreatSuccess()
+        {
+            return View("GreatSuccess");
+        }
+
+    }
+}
---
+++ Capstone.Web/Models/AddBeerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Web.Models
+{
+    public class AddBeerModel
+    {
+        public string BreweryName { get; set; }
+        public string BeerName { get; set; }
+        public string BeerTypeName { get; set; }
+        public string BeerDescription { get; set; }
+        public int ABV { get; set; }
+        public int IBU { get; set; }
+        public DateTime DateBrewed { get; set; }
+        public string BeerLabelImg { get; set; }
+    }
+}
