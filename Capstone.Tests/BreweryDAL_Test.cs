c24bd9904597ea34189b664c09c2a654e6cdd615
Jimmy Vanetta <j_vanetta@yahoo.com>
2018-04-16 16:03:47 -0400

    added unit test to test add brewery function

--- Capstone.Tests/BreweryDAL_Test.cs
+++ Capstone.Tests/BreweryDAL_Test.cs
@@ -18,6 +18,8 @@ namespace Capstone.Tests
             // Arrange
             BreweryDAL breweryDAL = new BreweryDAL(connectionString);
 
+            string name;
+
             using (TransactionScope transact = new TransactionScope())
             {
                 AddBreweryModel brewery = new AddBreweryModel
@@ -29,19 +31,27 @@ namespace Capstone.Tests
                     BreweryDistrict = "OH",
                     BreweryPostalCode = "44805",
                     History = "Making Home-Brews since 2016",
-                    YearFounded = 2016
+                    YearFounded = 2016,
+                    Email = "j_vanetta@yahoo.com",
+                    Phone = "419-631-6637",
+                    HoursOfOperation = "From Dusk til' Dawn",
+                    BreweryProfileImg = "none",
+                    BreweryBackgroundImg = "none",
+                    BreweryHeaderImage = "none"
                 };
 
                 breweryDAL.SetBreweryCoords(brewery);
 
                 breweryDAL.AddBrewery(brewery);
+
+                name = brewery.BreweryName;
+
+                // Act
+                List<string> results = breweryDAL.GetAllBreweryNames();
+
+                // Assert
+                Assert.IsTrue(results.Contains(name));
             }
-
-            // Act
-            List<string> results = breweryDAL.GetAllBreweryNames();
-
-            // Assert
-            Assert.IsTrue(results.Contains("Jimmy's Place"));
         }
     }
 }
