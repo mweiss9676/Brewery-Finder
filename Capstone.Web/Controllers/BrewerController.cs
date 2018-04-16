632f6171b1f950b2f9cb2fa73b6602473b8bde4b
Michael Weiss <mweis9676@gmail.com>
2018-04-16 11:32:08 -0400

    fixed a lot of the things with the brewer/index view and the css and the js

--- Capstone.Web/Controllers/BrewerController.cs
+++ Capstone.Web/Controllers/BrewerController.cs
@@ -1,9 +1,11 @@
 ﻿using Capstone.Web.DAL;
 using Capstone.Web.Models;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,12 +18,20 @@ namespace Capstone.Web.Controllers
         private IBreweryDAL breweryDAL;
         private IUserDAL userDAL;
 
+
+
         public BrewerController(IBeerDAL beerDAL, IBreweryDAL breweryDAL, IUserDAL userDAL)
         {
             this.beerDAL = beerDAL;
             this.breweryDAL = breweryDAL;
             this.userDAL = userDAL;
+            
         }
+
+        public ApplicationUserManager UserManager {  get => HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>(); }
+        
+        
+        
 
         // GET: Brewer
         public ActionResult Index()
@@ -39,6 +49,20 @@ namespace Capstone.Web.Controllers
         {
             breweryDAL.AddBrewery(brewery);
             return RedirectToAction("GreatSuccess");
+        }
+
+        
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> GrantRoleToUser(string username, string role)
+        {            
+            var user = await UserManager.FindByNameAsync(username);
+            user.Roles.Add(role);
+            await UserManager.UpdateAsync(user);
+
+            return RedirectToAction("blah");
         }
 
         [HttpPost]
